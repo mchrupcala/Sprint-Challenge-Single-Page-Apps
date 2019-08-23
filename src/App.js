9c412e1baa0a9e4808ba2b383985a1187a63c538
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-23 13:54:04 -0400

    Tried one of the stretch goals, importing the search feature, but I wasn't able to get it rendering.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {Route, NavLink} from 'react-router-dom';
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
-import CharacterList from "./components/CharacterList";
-import LocationsList from "./components/LocationsList";
-import EpisodeList from "./components/EpisodeList";
+// import CharacterList from "./components/CharacterList";
+// import LocationsList from "./components/LocationsList";
+// import EpisodeList from "./components/EpisodeList";
+// import SearchForm from "./components/SearchForm";
 
 
 export default function App() {
@@ -13,14 +14,15 @@ export default function App() {
     <main>
       <Header />
       <WelcomePage />
+      {/* <SearchForm /> */}
       <TabNav />
       {/* <CharacterList /> */}
       {/* <LocationsList /> */}
       {/* <EpisodeList /> */}
 
     {/* <Route path="/CharacterList" component={CharacterList} /> */}
-    <Route path="/LocationsList" component={LocationsList} />
-    <Route path="/EpisodeList" component={EpisodeList} />
+    {/* <Route path="/LocationsList" component={LocationsList} />
+    <Route path="/EpisodeList" component={EpisodeList} /> */}
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 
 export default function SearchForm({ onSearch }) {
   // STRETCH TODO: Add stateful logic for query/form data
+
+  const [newValue, setNewValue] = useState(null);
+
+function handleInputChange() {
+  const target = event.target;
+  const value = target.value;
+  const name = target.name;
+
+  return (
+    setNewValue(value)
+  )
+
+}
+
   return (
     <section className="search-form">
       <form onSubmit={() => onSearch(name)}>
+
         <input
           onChange={handleInputChange}
           placeholder="name"
           value={name}
           name="name"
         />
+
         <button type="submit">Search</button>
       </form>
     </section>
